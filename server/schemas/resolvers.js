08bead21843f7cbb20ecb895692816ad1f280b1b
Justin <dong135790@gmail.com>
2023-09-19 16:51:44 -0400

    resolvers and typeDefs

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,4 +1,5 @@
 const { GraphQLError } = require('graphql');
+const { signToken, AuthenticationError } = require('../utils/auth');
 const { User } = require('../models')
 const resolvers = {
     Query: {
@@ -10,9 +11,55 @@ const resolvers = {
             }
 
             return foundUsers;
+        },
+    // By adding context to our query, we can retrieve the logged in user without specifically searching for them
+        me: async (parent, args, context) => {
+            if (context.user) {
+                return User.findOne({ _id: context.user._id})
+            }
+            throw AuthenticationError
         }
     },
-    // Mutation: {},
+    Mutation: {
+        login: async (parent, { email, password }) => {
+            const user = await User.findOne({ email });
+            if(!user) {
+                throw AuthenticationError;
+            }
+
+            // Checking if password is correct
+            const correctPw = await User.isCorrectPassword(password);
+            if (!correctPw) {
+                throw AuthenticationError;
+            }
+
+            const token = signToken(user);
+            return { token, profile };
+        },
+        addUser: async (parent, { name, email, password }) => {
+            const user = await User.create({ name, email, password });
+            const token = signToken(user);
+
+            return { token, user };
+        },
+        saveBook: async (parent, { authors, description, title, bookId , image, link }, context) => {
+            if (context.user) {
+                return User.findOneAndUpdate(
+                    {  }
+                )
+            }
+        },
+        removeBook: async (parent, { bookId }, context) => {
+            if (context.user) {
+                return User.findOneAndUpdate(
+                    { _id: context.user._id },
+                    { $pull: {savedBooks: bookId}},
+                    { new: true}
+                );
+            }
+            throw AuthenticationError
+        }
+    },
 
 }
 
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -6,6 +6,7 @@ const typeDefs = `#graphql
         password: String
         savedBooks: [Book]
     }
+
     type Book {
         _id: ID
         authors: [String]
@@ -15,8 +16,22 @@ const typeDefs = `#graphql
         link: String
         title: String
     }
+
+    type Auth {
+        token: ID!
+        user: User
+    }
+
     type Query {
         getAllusers: [User]
+        me: User
+    }
+
+    type Mutation {
+        login(email: String!, password: String!): Auth
+        addUser(name: String!, email: String!, password: String!): Auth
+        saveBook(authors: String!, description: String!, title: String!, bookId: String!, image: String!, link: String!) : User
+        removeBook(bookId: String!): User
     }
 `
 
